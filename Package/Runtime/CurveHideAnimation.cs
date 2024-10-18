aa3f769c6eb83a2d5314a9a5738db8328e1e5a4f
Luca3317 <lweist3317@gmail.com>
2024-10-18 13:43:29 +0200

    Add basic show/hide animation that uses basic animation w/ curve

---
+++ Package/Runtime/CurveHideAnimation.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPEffects.CharacterData;
+using TMPEffects.TMPAnimations;
+using UnityEngine;
+
+namespace TMPEffects.TMPAnimations.ShowAnimations
+{
+    [CreateAssetMenu(fileName = "new CurveHideAnimation", menuName = "TMPEffects/Animations/Hide Animations/Curve")]
+    public class CurveHideAnimation : TMPHideAnimation
+    {
+        public TMPAnimation animation;
+        public AnimationCurve curve;
+        public float duration;
+
+        public override void Animate(CharData cData, IAnimationContext context)
+        {
+            if (animation == null) return;
+
+            float passed = context.AnimatorContext.PassedTime - context.AnimatorContext.StateTime(cData);
+            float t = curve.Evaluate(passed / duration);
+            if (passed > duration)
+            {
+                context.FinishAnimation(cData);
+                return;
+            }
+
+            animation.Animate(cData, context);
+            CharDataModifiers.LerpCharacterModifiersUnclamped(cData, cData.CharacterModifiers, t,
+                cData.CharacterModifiers);
+            CharDataModifiers.LerpMeshModifiersUnclamped(cData, cData.MeshModifiers, t, cData.MeshModifiers);
+        }
+
+        public override bool ValidateParameters(IDictionary<string, string> parameters)
+        {
+            if (animation == null) return false;
+            return animation.ValidateParameters(parameters);
+        }
+
+        public override void SetParameters(object customData, IDictionary<string, string> parameters)
+        {
+            if (animation == null) return;
+            animation.SetParameters(customData, parameters);
+        }
+
+        public override object GetNewCustomData()
+        {
+            if (animation == null) return null;
+            return animation.GetNewCustomData();
+        }
+    }
+}
